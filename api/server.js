c1a06a1bd8f2a7df455a1159e462d109a70047cf
thanhlamcode <144249019+thanhlamcode@users.noreply.github.com>
2024-10-17 09:59:03 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,31 +1,39 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
 
-const server = jsonServer.create()
+// Tạo server JSON
+const server = jsonServer.create();
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+// Thiết lập đường dẫn tới tệp JSON
+const filePath = path.join(__dirname, 'db.json');
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+// Đọc nội dung từ tệp JSON
+const data = fs.readFileSync(filePath, 'utf-8');
+const db = JSON.parse(data);
 
-const middlewares = jsonServer.defaults()
+// Tạo router từ nội dung tệp JSON (cho phép ghi dữ liệu)
+const router = jsonServer.router(filePath); // Sử dụng file trực tiếp cho phép ghi dữ liệu
 
-server.use(middlewares)
-// Add this before server.use(router)
+const middlewares = jsonServer.defaults();
+
+// Áp dụng middleware
+server.use(middlewares);
+
+// Rewriter để thay đổi URL API
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+}));
 
-// Export the Server API
-module.exports = server
+// Sử dụng router đã được cấu hình
+server.use(router);
+
+// Khởi động server trên cổng 3000
+server.listen(3000, () => {
+    console.log('JSON Server is running and allows writing data');
+});
+
+// Export server để có thể sử dụng ở nơi khác nếu cần
+module.exports = server;
